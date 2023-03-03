48f59779875aca1cb24aba49e6fe7858d64f852e
samtgdk0 <sammtgdk0@gmail.com>
2023-03-03 17:08:05 +0300

    basic increase decrease button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="window">
+
+    <h1>sayim:{{sayi}}</h1>
+    <div class="d-flex gap-2">
+       <button @click="arttir(5)" class="btn btn-success">Arttır</button>
+       <button @click="azalt(2)" class="btn btn-danger">Azalt</button>
+    </div>
+   
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+
+
+export default{
+
+  name:'app',
+  data() {
+    return {
+      isim:'MERT',
+      soyisim:'ulusoy',
+      yas:20,
+      a:10,
+      sayi:0,
+    }
+  },
+  methods: {
+    arttir(kackac){
+      this.sayi = this.sayi +kackac;
+    },
+    azalt(kackac){
+      this.sayi = this.sayi - kackac;
+    }
+  },
+  mounted() {
+    
+  },
+   
 }
+
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.window{
+  width: 100%;
+  margin-top: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
